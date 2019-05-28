73c81457f8aec3c2ae1af9f226f54da873fb8ec8
Minal Kharche <minal.berkeley@gmail.com>
2019-05-28 08:52:03 -0700

    added random number array

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+var randomNum = [];
+for(var i=19; i<121; i++){
+    randomNum.push(i);
+}
+console.log(randomNum);
+
+var crystals = [];
+for(var j=1; j<13; j++){
+    crystals.push(j);
+}
+console.log(crystals);
+
+
+});
